fd27985dbbeaac22bc129a06015303faf0119f61
David31rm <102294846+David31rm@users.noreply.github.com>
2023-04-03 19:41:59 +0100

    Update PingPongEndpoint.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
@@ -28,4 +28,8 @@ public class PingPongEndpoint {
 	public String add() {
 		return grpcClientService.add();
 	}
+	 @GetMapping("/multiplication")
+	public String add() {
+		return grpcClientService.add();
+	}
 }
